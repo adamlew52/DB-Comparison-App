f56c554c325f42e1353c95d887edd873b99800dc
adamlew52 <95544948+adamlew52@users.noreply.github.com>
2022-04-07 23:59:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import dbControl
+import TrendFinder
 
 #db1Name = input("Please enter the name of the first db you would like to compare: ")
 #db2Name = input("Please enter the name of the second db you would like to compare: ")
@@ -15,6 +16,10 @@ dbData2Array = dbControl.parseData(dbData2)
 # ask the user what they would like to compare
 # ^include^ display the types of data being stored in the db's
 
-dataName1 = "data1"
-dataName2 = "data2"
-dbControl.compareData(dataName1,dataName2,db1Name,db2Name,dbData1Array,dbData2Array)
+#dataName1 = "data1"
+#dataName2 = "data2"
+#dbControl.compareData(dataName1,dataName2,db1Name,db2Name,dbData1Array,dbData2Array)
+
+
+#Trend Finder processing
+#TrendFinder.findDifferences(dbData1Array, dbData2Array)
